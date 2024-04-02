992c4a8249b752680bd0c7e4c8056ae57cb843a3
Bend <144515341+APIDT@users.noreply.github.com>
2024-04-02 20:10:07 +0300

    event.js

--- event.js
+++ event.js
@@ -1,17 +1,30 @@
 const fs = require('fs');
+const EventEmitter = require('events');
+
+const eventEmitter = new EventEmitter();
 
 function readTextFile(fileRead) {
-
-    fs.readFile(fileRead, 'utf8', (err,  success) => {
+    fs.readFile(fileRead, 'utf8', (err, success) => {
         if (err) {
-            console.error('Error reading file', err);
+            console.error('Error reading file:', err);
             return;
         }
         
-        console.log('File content successfully retrieved:',  success);
+        console.log('File content successfully retrieved:', success); 
     });
 }
 
 const fileRead = 'text.text';
 
 readTextFile(fileRead);
+
+eventEmitter.on('readFile', (success) => {
+    console.log('File has been read successfully:', success);
+});
+
+
+eventEmitter.on('error', (err) => {
+    console.error('Error occurred:', err);
+});
+
+eventEmitter.emit('readFile', 'text.text'); 
